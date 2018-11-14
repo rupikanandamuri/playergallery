bcacfa93596fa9638bbb57d00c0134d63bbf5d3e
Rupika Sompalli <rupikasompalli@rupikas-mac-mini.local>
2018-11-14 14:44:19 +0530

    Initial Commit

---
+++ playerGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playerGallery
+//
+//  Created by Rupika Sompalli on 14/11/18.
+//  Copyright © 2018 Rupika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
